444e21420886a2a304d6cc09a2cd9d811e269db4
EliiStark <94858181+EliiStark@users.noreply.github.com>
2023-10-23 16:57:34 +0200

    Added start of Voronoi demo
    Added a random point generated script

---
+++ Assets/Scripts/Voronoi_v1/GeneratePoints.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class GeneratePoints : MonoBehaviour
+{
+   // [SerializeField] private GameObject prefabPoint;
+    [SerializeField] private int numPoints;
+    private Vector3[] arrayPoints;  //point coordenates array
+    private float sizeX;
+    private float sizeZ;
+    
+    void Start()
+    {
+        
+        Mesh planeMesh = this.GetComponent<MeshFilter>().mesh;
+        arrayPoints = new Vector3[numPoints];
+
+        //Get the size of the plane in pixels
+        sizeX = this.transform.localScale.x * planeMesh.bounds.size.x;
+        Debug.Log(sizeX);
+        sizeZ = this.transform.localScale.z * planeMesh.bounds.size.z;
+        Debug.Log(sizeZ);
+
+
+        PlacePoints();
+    }
+
+
+    private void PlacePoints()
+    {
+        //intantiate a point numPoint number of times
+        for(int i=0; i< numPoints; i++)
+        {
+            float x = Random.Range(this.transform.position.x - sizeX / 2, this.transform.position.x + sizeX / 2);
+            float z = Random.Range(this.transform.position.z - sizeZ / 2, this.transform.position.z + sizeZ / 2);
+
+            arrayPoints[i] = new Vector3(x, this.transform.position.y, z);
+            Debug.Log(arrayPoints[i]);
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+//this #if #endif is for not throwing an error while playing the demo
+#if UNITY_EDITOR
+        if (EditorApplication.isPlaying)
+        {
+            for (int i = 0; i < numPoints; i++)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(arrayPoints[i], 0.1f);
+            }
+        }
+#endif
+
+    }
+}
